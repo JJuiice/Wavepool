c2974db48658c58222cb84db94d44f578ce4bd10
Ojas Anand <oanand@fastmail.com>
2022-04-19 14:44:47 -0400

    Added radio buttons for selecting operation to execute

---
+++ src/public/js/controlFormVisibility.js
@@ -0,0 +1,7 @@
+const ENC_RDIO_BTN = document.querySelector("form>.actSelInput>#enc");
+const DEC_RDIO_BTN = document.querySelector("form>.actSelInput>#dec");
+
+window.onload = (event) => {
+  console.log(ENC_RDIO_BTN.value);
+  console.log(DEC_RDIO_BTN.value);
+};
